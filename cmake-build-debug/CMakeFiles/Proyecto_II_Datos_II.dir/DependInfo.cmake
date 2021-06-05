27103ef07f7efa2a1cdf7ba4713a887d1c09eeaa
Joel Gómez <62965696+Joel-Araya@users.noreply.github.com>
2021-06-04 18:18:58 -0600

    Se corrigieron errores

---
+++ cmake-build-debug/CMakeFiles/Proyecto_II_Datos_II.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/joel/Escritorio/Proyecto_II_Datos_II/main.cpp" "/home/joel/Escritorio/Proyecto_II_Datos_II/cmake-build-debug/CMakeFiles/Proyecto_II_Datos_II.dir/main.cpp.o"
+  "/home/joel/Escritorio/Proyecto_II_Datos_II/src/Backtraking/Backtracking.cpp" "/home/joel/Escritorio/Proyecto_II_Datos_II/cmake-build-debug/CMakeFiles/Proyecto_II_Datos_II.dir/src/Backtraking/Backtracking.cpp.o"
+  "/home/joel/Escritorio/Proyecto_II_Datos_II/src/PathfindingA/PathfindingAStar.cpp" "/home/joel/Escritorio/Proyecto_II_Datos_II/cmake-build-debug/CMakeFiles/Proyecto_II_Datos_II.dir/src/PathfindingA/PathfindingAStar.cpp.o"
+  "/home/joel/Escritorio/Proyecto_II_Datos_II/src/PathfindingA/StarNode.cpp" "/home/joel/Escritorio/Proyecto_II_Datos_II/cmake-build-debug/CMakeFiles/Proyecto_II_Datos_II.dir/src/PathfindingA/StarNode.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Proyecto_II_Datos_II.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto_II_Datos_II.dir/main.cpp.o"
+  "CMakeFiles/Proyecto_II_Datos_II.dir/src/Backtraking/Backtracking.cpp.o"
+  "CMakeFiles/Proyecto_II_Datos_II.dir/src/PathfindingA/PathfindingAStar.cpp.o"
+  "CMakeFiles/Proyecto_II_Datos_II.dir/src/PathfindingA/StarNode.cpp.o"
+  "Proyecto_II_Datos_II"
+  "Proyecto_II_Datos_II.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_II_Datos_II.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
